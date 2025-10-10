f9e2569d86a25bfca2c9a5b84e143278da88467c
Carlotta <juan.rubio262132@potros.itson.edu.mx>
2025-10-10 16:46:39 -0700

    Creacion de velote

---
+++ entities/palindrome.py
@@ -0,0 +1,13 @@
+class palindrome:
+
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+        def is_palindrome(self) -> bool:
+            p = self.phrase
+            #si es palindromo retorna True y si no False
+
+            if p == palindrome:
+                return True
+            else:
+                return False
